f3021c6a069a881853fca5bce01364802d74cb41
Solipsist17 <sebas.17v@gmail.com>
2024-11-04 09:04:30 -0500

    agregando cors

--- foro/src/main/java/com/alura/foro/ForoApplication.java
+++ foro/src/main/java/com/alura/foro/ForoApplication.java
@@ -2,12 +2,29 @@ package com.alura.foro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ForoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Esto aplica CORS a todas las rutas.
+						//.allowedOrigins("http://localhost:3000") // Orígenes permitidos
+						.allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+						.allowedHeaders("*");
+						// .allowedHeaders("Content-Type", "Authorization") // solo encabezados permitidos
+			}
+		};
 	}
 
 }
